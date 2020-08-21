dad75c14784f7274c280b27b4ffc92cf488dafdd
yarrib76 <yarrib76@gmail.com>
2020-08-21 00:47:16 +0000

    Problemas con Replica Inteligente

--- app/Http/Controllers/Api/ABMTiendaNubeNew.php
+++ app/Http/Controllers/Api/ABMTiendaNubeNew.php
@@ -132,7 +132,7 @@ class ABMTiendaNubeNew extends Controller
     }
 
     public function sincroArticulos(){
-        dd("Hola");
+        dd("Hola"); //p
         $id_corrida = Input::get('id_corrida');
         $store_id = Input::get('store_id');
         $conOrden = Input::get('conOrden');
