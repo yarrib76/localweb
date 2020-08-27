819d3665e6771a519cae1110a9acacdc31f1f0ca
Vagrant <vagrant@gmail.com>
2020-08-27 17:20:41 +0000

    Mejora Articulos

---
+++ app/Http/Controllers/Api/Articulos/GetArticulos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Donatella\Http\Controllers\Api\Articulos;
+
+use Illuminate\Http\Request;
+
+use Donatella\Http\Requests;
+use Donatella\Http\Controllers\Controller;
+
+class GetArticulos extends Controller
+{
+    //
+}
