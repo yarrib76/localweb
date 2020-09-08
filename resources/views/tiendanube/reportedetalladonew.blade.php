3528b614b9b2b511f9acbf4cf490a4a6fd35b442
yarrib76 <yarrib76@gmail.com>
2020-09-08 01:10:26 +0000

    Agrego cantidad de ordenes

--- resources/views/tiendanube/reportedetalladonew.blade.php
+++ resources/views/tiendanube/reportedetalladonew.blade.php
@@ -34,9 +34,9 @@
                                 <option value="10">10</option>
                                 <option value="15">15</option>
                                 <option value="20">20</option>
-                                <option value="30">20</option>
-                                <option value="40">20</option>
-                                <option value="50">20</option>
+                                <option value="30">30</option>
+                                <option value="40">40</option>
+                                <option value="50">50</option>
                             </select>
                             Ordenes de Compra |
                         </label>
