bf2fc0718d25fd0f15ca21d12e464dcf19ce4054
Vagrant <vagrant@gmail.com>
2021-03-10 12:53:25 +0000

    Cree ControlCierreDiario

---
+++ app/Http/Controllers/CierreDiario/ControlCierreDiario.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Donatella\Http\Controllers\CierreDiario;
+
+use Illuminate\Http\Request;
+
+use Donatella\Http\Requests;
+use Donatella\Http\Controllers\Controller;
+
+class ControlCierreDiario extends Controller
+{
+    //
+}
