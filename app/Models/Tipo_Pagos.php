88fbd24409fb58816e60004c38c0b4eb6997384d
Vagrant <vagrant@gmail.com>
2021-04-27 16:53:18 +0000

    Facturas estado Financiera

--- app/Models/Tipo_Pagos.php
+++ app/Models/Tipo_Pagos.php
@@ -4,7 +4,7 @@ namespace Donatella\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Tipo_pagos extends Model
+class Tipo_Pagos extends Model
 {
     protected $table = 'tipo_pagos';
     protected $fillable = ['id_tipo_pagos','tipo_pago'];
