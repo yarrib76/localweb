f662b5a8ec721c3aa598d8efe33d7c6d49b33f34
Yamil <yarrib76@gmail.com>
2022-12-14 19:37:20 -0300

    Agrego viamoreprodlaptop en servidor para envío de mail replica

--- app/Http/Controllers/Mail/ServerStatusMail.php
+++ app/Http/Controllers/Mail/ServerStatusMail.php
@@ -26,6 +26,10 @@ class ServerStatusMail extends Controller
                 $mysqliConti = new mysqli("192.168.0.110", "yarrib76", "NetAcc10", "samira");
                 $mysqliProd = new mysqli("192.168.0.104", "yarrib76", "NetAcc10", "samira");
                 break;
+            case 'viamoreprodlaptop':
+                $mysqliConti = new mysqli("192.168.0.110", "yarrib76", "NetAcc10", "samira");
+                $mysqliProd = new mysqli("192.168.0.104", "yarrib76", "NetAcc10", "samira");
+                break;
             case 'donaprod':
                 $mysqliConti = new mysqli("192.168.0.150", "root", "NetAcc10", "samira");
                 $mysqliProd = new mysqli("192.168.0.50", "root", "NetAcc10", "samira");
