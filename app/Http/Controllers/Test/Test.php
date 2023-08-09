b1850bf5e0a9be8d88ff6b290403f65573c33616
Yamil <yarrib76@gmail.com>
2023-08-09 10:37:11 -0300

    Primer Commit diasNoTrabajados

--- app/Http/Controllers/Test/Test.php
+++ app/Http/Controllers/Test/Test.php
@@ -24,7 +24,7 @@ use mysqli;
 class Test extends Controller
 {
     //Commit DashBoard..
-    //Primer Commit ExportFacturas
+    //Primer Commit diasNoTrabajdos
     public function Test()
     {
         $path = 'public/export/facturas/';
