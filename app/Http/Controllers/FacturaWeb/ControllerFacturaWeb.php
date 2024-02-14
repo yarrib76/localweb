b8ba132916cfabb70740f12da21bfec8787164be
Yamil <yarrib76@gmail.com>
2024-02-14 14:08:17 -0300

    FacturaWeb, prueba de IPCliente

--- app/Http/Controllers/FacturaWeb/ControllerFacturaWeb.php
+++ app/Http/Controllers/FacturaWeb/ControllerFacturaWeb.php
@@ -30,7 +30,6 @@ class ControllerFacturaWeb extends Controller
     {
         $nameCajera = Auth::user()->name;
         $clienteDireccionIP = $request->ip();
-        dd($clienteDireccionIP);
         $auto = $this->autorizacionFacturaWeb($clienteDireccionIP);
         if ($auto){
             $control = 'Autorizado';
