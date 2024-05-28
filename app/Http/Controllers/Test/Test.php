b58c9f008100753779ddc8a2233befcb7cf68483
Yamil <yarrib76@gmail.com>
2024-05-28 17:52:26 -0300

    Primer Commit GPT

--- app/Http/Controllers/Test/Test.php
+++ app/Http/Controllers/Test/Test.php
@@ -25,7 +25,7 @@ class Test extends Controller
 {
     public function Test()
     {
-        //Priemer Commit miCorreoPedidosPagos
+        //Priemer Commit ApiGPT
 
         return view('test.pdf');
         $path = 'public/export/facturas/';
