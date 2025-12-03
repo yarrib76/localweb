a9073f5c6e5e7c74f5235add7ad7bf552c443c59
Yamil <yarrib76@gmail.com>
2025-12-03 16:38:52 -0300

    Quito permisos de la vista de clientes, para que el tipo 4 no acceda

--- app/Http/Controllers/Cliente/ClientesController.php
+++ app/Http/Controllers/Cliente/ClientesController.php
@@ -17,7 +17,8 @@ class ClientesController extends Controller
 {
     public function __construct()
     {
-                $this->middleware('auth');
+        $this->middleware('auth');
+        $this->middleware('role:Gerencia,Caja');
     }
     /**
      * Display a listing of the resource.
